3dc18a66cb6713db922bbc7e906d5269aa152f6a
Javier Cuenca <javier.cuenca.6@gmail.com>
2024-09-11 13:26:12 +0200

    lists comps

--- app/(main)/competition/[id]/alphabetical-list/page.tsx
+++ app/(main)/competition/[id]/alphabetical-list/page.tsx
@@ -2,13 +2,17 @@ import { fetchCompetitionData } from "@/lib/api"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { ArrowLeft } from "lucide-react"
 
 export default async function AlphabeticalListPage({ params: { id } }: { params: { id: string } }) {
   const data = await fetchCompetitionData(id)
   const alphabeticallySortedQualification = [...data.qualification].sort((a, b) => a.athlete.localeCompare(b.athlete))
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <>
+    <Card>
+      <CardContent className="p-6">
       <h1 className="text-2xl font-bold mb-4">Listado Alfabético</h1>
       <Table>
         <TableHeader>
@@ -28,9 +32,12 @@ export default async function AlphabeticalListPage({ params: { id } }: { params:
           ))}
         </TableBody>
       </Table>
+
+      </CardContent>
+    </Card>
       <Link href={`/competition/${id}`} passHref>
-        <Button className="mt-4">Volver</Button>
+        <Button className="mt-4"><ArrowLeft /> Volver</Button>
       </Link>
-    </div>
+    </>
   )
 }
--- app/(main)/competition/[id]/target-list/page.tsx
+++ app/(main)/competition/[id]/target-list/page.tsx
@@ -2,14 +2,18 @@ import { fetchCompetitionData } from "@/lib/api"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { ArrowLeft } from "lucide-react"
 
 export default async function TargetListPage({ params: { id } }: { params: { id: string } }) {
   const data = await fetchCompetitionData(id)
   const sortedQualification = [...data.qualification].sort((a, b) => a.target.localeCompare(b.target))
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-4">Listado por Diana</h1>
+    <>
+    <Card>
+      <CardContent className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Listado por Dianas</h1>
       <Table>
         <TableHeader>
           <TableRow>
@@ -28,9 +32,12 @@ export default async function TargetListPage({ params: { id } }: { params: { id:
           ))}
         </TableBody>
       </Table>
+
+      </CardContent>
+    </Card>
       <Link href={`/competition/${id}`} passHref>
-        <Button className="mt-4">Volver</Button>
+        <Button className="mt-4"><ArrowLeft /> Volver</Button>
       </Link>
-    </div>
+      </>
   )
 }
